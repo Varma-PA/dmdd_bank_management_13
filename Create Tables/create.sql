68266849b9048d4c2d95403c7668d34becca43f5
Hazel Lobo <lobo.ha@northeastern.edu>
2022-07-26 16:54:06 -0400

    Table creation changes

--- Create Tables/create.sql
+++ Create Tables/create.sql
@@ -1,137 +1,151 @@
+/* Team 13
+INFO 6210 Database Management and Database Design
+P4
+Submitted by 
+Sree Achyutha Penmetcha           NUID : 
+Ankita Kumari                     NUID : 
+Hazel Lobo						  NUID : 001001740
+Sanjana Chatti                    NUID : 
+Vachana Belgai                    NUID : 
+*/
 
-CREATE TABLE CustomerData(
-    CustomerID INT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    DateOfBirth DATE,
-    SSN VARCHAR(20),
-    Email VARCHAR(20),
-    PhoneNumber VARCHAR(15),
-    HomeAddress VARCHAR(15),
-    City VARCHAR(10),
-    State VARCHAR(20),
-    ZipCode VARCHAR(10)
+-- Creation of Tables for our Database Implementation
+
+--Create database
+CREATE DATABASE BankManagementSystem;
+
+USE BankManagementSystem;
+
+--Table Branch
+
+CREATE TABLE BankBranch(
+    BranchCode INT NOT NULL PRIMARY KEY,
+    BranchName VARCHAR(20) NOT NULL,
+	PhoneNumber CHAR(10) NOT NULL CHECK (PhoneNumber like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+    Email VARCHAR(20) NOT NULL,
+    [Address] VARCHAR(50) NOT NULL,
+    City VARCHAR(10) NOT NULL,
+    [State] VARCHAR(15) NOT NULL,
+	ZipCode CHAR(5) NOT NULL CHECK (ZipCode like '[0-9][0-9][0-9][0-9][0-9]')
 );
 
+-- Table Person
+CREATE TABLE Person(
+    PersonID INT NOT NULL PRIMARY KEY,
+    FirstName VARCHAR(20) NOT NULL,
+    LastName VARCHAR(20),
+    DateOfBirth DATE NOT NULL,
+    SSN VARCHAR(50),
+    Email VARCHAR(20),
+	PhoneNumber CHAR(10) NOT NULL CHECK (PhoneNumber like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+    [Address] VARCHAR(MAX) NOT NULL,
+    City VARCHAR(20) NOT NULL,
+	[State] VARCHAR(15) NOT NULL,
+	ZipCode CHAR(5) NOT NULL CHECK (ZipCode like '[0-9][0-9][0-9][0-9][0-9]')
+);
+
+--Table Employee Type
+CREATE TABLE EmployeeType(
+    EmployeeTypeID INT NOT NULL PRIMARY KEY,
+    [Type] VARCHAR(10) NOT NULL
+);
+
+--Table Employee
+CREATE TABLE Employee(
+    EmployeeID INT NOT NULL PRIMARY KEY,
+    EmployeeTypeID INT NOT NULL FOREIGN KEY REFERENCES EmployeeType(EmployeeTypeID),
+    BranchCode INT NOT NULL FOREIGN KEY REFERENCES BankBranch(BranchCode),
+    PersonID INT NOT NULL FOREIGN KEY REFERENCES Person(PersonID)
+);
+
+--Table Customer Data
+CREATE TABLE CustomerData(
+    CustomerID INT NOT NULL PRIMARY KEY,
+    PersonID INT NOT NULL FOREIGN KEY REFERENCES Person(PersonID)
+);
+
+--Table Customer Financial History
 CREATE TABLE CustomerFinancialHistory(
-    FinancialHistoryID INT PRIMARY KEY,
-    CustomerID INT FOREIGN KEY REFERENCES CustomerData(CustomerID),
+    FinancialHistoryID INT NOT NULL PRIMARY KEY,
+    CustomerID INT NOT NULL FOREIGN KEY REFERENCES CustomerData(CustomerID),
     CreditScore INT,
-    Status VARCHAR(10),
+    [Status] VARCHAR(10),
     LastUpdatedTime DATE
 );
 
-CREATE TABLE CardType(
-    CardTypeID INT PRIMARY KEY,
-    TYPE VARCHAR(10),
-    CardDescription VARCHAR(10)
-);
-
-CREATE TABLE BankBranch(
-    BranchCode INT PRIMARY KEY,
-    BranchName VARCHAR(20),
-    PhoneNumber VARCHAR(12),
-    Email VARCHAR(20),
-    Address VARCHAR(50),
-    City VARCHAR(10),
-    State VARCHAR(15),
-    ZipCode VARCHAR(10)
-);
-
-CREATE TABLE EmployeeType(
-    EmployeeTypeID INT PRIMARY KEY,
-    Type VARCHAR(10)
-);
-
--- CREATE TABLE Employee(
---     EmployeeID INT PRIMARY KEY,
---     EmployeeType INT FOREIGN KEY REFERENCES EmployeeType(EmployeeTypeID),
---     BranchCode INT FOREIGN KEY REFERENCES BankBranch(BranchCode),
---     FirstName VARCHAR(20),
---     LastName VARCHAR(20),
---     DateOfBirth DATE
--- )
-
-CREATE TABLE InsuranceType(
-    InsuranceTypeID INT PRIMARY KEY,
-    InsuranceType VARCHAR(20),
-    InsuranceDescription VARCHAR(20)
-);
-
-CREATE TABLE LoanType(
-    LoanTypeID INT PRIMARY KEY,
-    LoanType VARCHAR(20),
-    LoanDescription VARCHAR(50)
-);
-
--- Created Person class for redundancy
-CREATE TABLE Person(
-    PersonID INT PRIMARY KEY,
-    FirstName VARCHAR(20),
-    LastName VARCHAR(20),
-    DateOfBirth DATE,
-    SSN VARCHAR(50),
-    Email VARCHAR(20),
-    PhoneNumber VARCHAR(12),
-    Address VARCHAR(MAX),
-    City VARCHAR(20),
-    State VARCHAR(20),
-    ZipCode VARCHAR(10)
-);
-
-CREATE TABLE CustomerData(
-    CustomerID INT PRIMARY KEY,
-    PersonID INT FOREIGN KEY REFERENCES Person(PersonID)
-);
-
-CREATE TABLE Employee(
-    EmployeeID INT PRIMARY KEY,
-    EmployeeTypeID INT FOREIGN KEY REFERENCES EmployeeType(EmployeeTypeID),
-    BranchCode INT FOREIGN KEY REFERENCES BankBranch(BranchCode),
-    PersonID INT FOREIGN KEY REFERENCES Person(PersonID)
-);
-
+--Table Account
 CREATE TABLE Account(
-    AccountID INT PRIMARY KEY,
-    BranchCode INT FOREIGN KEY REFERENCES BankBranch(BranchCode),
-    CustomerID INT FOREIGN KEY REFERENCES CustomerData(CustomerID),
-    CREATEDBYEmployee INT FOREIGN KEY REFERENCES Employee(EmployeeID),
-    AccountType VARCHAR(10),
+    AccountID INT NOT NULL PRIMARY KEY,
+    BranchCode INT NOT NULL FOREIGN KEY REFERENCES BankBranch(BranchCode),
+    CustomerID INT NOT NULL FOREIGN KEY REFERENCES CustomerData(CustomerID),
+    CreatedByEmployee INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeID),
+    AccountType VARCHAR(10) NOT NULL,
     Balance MONEY DEFAULT 0.0
 );
 
+--Table Insurance Type
 CREATE TABLE InsuranceType(
-    InsuranceTypeID INT PRIMARY KEY,
-    InsuranceName VARCHAR(10),
+    InsuranceTypeID INT NOT NULL PRIMARY KEY,
+    InsuranceName VARCHAR(10) NOT NULL,
     InsuranceDescription VARCHAR(50)
 );
 
-
+--Table Insurance
 CREATE TABLE Insurance(
-    InsuranceID INT PRIMARY KEY,
-    AccountID INT FOREIGN KEY REFERENCES Account(AccountID),
-    InsuranceTypeID INT FOREIGN KEY REFERENCES InsuranceType(InsuranceTypeID),
-    ApprovedByEmployee INT FOREIGN KEY REFERENCES Employee(EmployeeID),
-    InsuranceName VARCHAR(20),
+    InsuranceID INT NOT NULL PRIMARY KEY,
+    AccountID INT NOT NULL FOREIGN KEY REFERENCES Account(AccountID),
+    InsuranceTypeID INT NOT NULL FOREIGN KEY REFERENCES InsuranceType(InsuranceTypeID),
+    ApprovedByEmployee INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeID),
     InsuranceAmount MONEY,
-    Status VARCHAR(20)
+    [Status] VARCHAR(20)
 );
 
-
+--Table Loan Type
 CREATE TABLE LoanType(
-    LoanTypeID INT PRIMARY KEY,
-    LoanType VARCHAR(10),
+    LoanTypeID INT NOT NULL PRIMARY KEY,
+    LoanName VARCHAR(20) NOT NULL,
     LoanDescription VARCHAR(50)
 );
 
+--Table Loan
 CREATE TABLE Loan(
-    LoanID INT PRIMARY KEY,
-    AccountID INT FOREIGN KEY REFERENCES Account(AccountID),
-    LoanTypeID INT FOREIGN KEY REFERENCES LoanType(LoanTypeID),
-    LoanName VARCHAR(20),
-    LoanAmount MONEY,
-    LoanPaid MONEY,
+    LoanID INT NOT NULL PRIMARY KEY,
+    AccountID INT NOT NULL FOREIGN KEY REFERENCES Account(AccountID),
+    LoanTypeID INT NOT NULL FOREIGN KEY REFERENCES LoanType(LoanTypeID),
+	ApprovedByEmployee INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeID),
+    LoanAmount MONEY DEFAULT 0.0,
+    LoanPaid MONEY DEFAULT 0.0,
     RecentPaymentDate DATE,
     LoanDisbursed MONEY,
     RecentDisbursementDate DATE
 );
+
+--Table Card Type
+CREATE TABLE CardType(
+    CardTypeID INT NOT NULL PRIMARY KEY,
+    CardName VARCHAR(20) NOT NULL,
+	CardProvider VARCHAR(20),
+	InterestRate DECIMAL(2,2),
+    CardDescription VARCHAR(10)
+);
+
+--Table Card
+CREATE TABLE [Card](
+    CardID INT NOT NULL PRIMARY KEY,
+    AccountID INT NOT NULL FOREIGN KEY REFERENCES Account(AccountID),
+    CardTypeID INT NOT NULL FOREIGN KEY REFERENCES CardType(CardTypeID),
+	ApprovedByEmployee INT NOT NULL FOREIGN KEY REFERENCES Employee(EmployeeID),
+    Balane MONEY DEFAULT 0.0,
+    [Status] VARCHAR(10) NOT NULL
+);
+
+--Table Transaction
+CREATE TABLE [Transaction](
+	TransactionID INT NOT NULL PRIMARY KEY,
+    AccountID INT FOREIGN KEY REFERENCES Account(AccountID),
+    CardID INT FOREIGN KEY REFERENCES [Card](CardID),
+    InsuranceID INT FOREIGN KEY REFERENCES Insurance(InsuranceID),
+	BeneficiaryAccountID INT FOREIGN KEY REFERENCES Account(AccountID),
+    Amount MONEY NOT NULL,
+    TransactionDate VARCHAR(10) NOT NULL
+)
