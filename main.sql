17d6d1eba57e649f1c9ea98a182b13e302507d30
Ankita Kumari <kumari.ankita89@gmail.com>
2022-07-27 21:22:40 -0700

    added NUID

--- main.sql
+++ main.sql
@@ -3,7 +3,7 @@ INFO 6210 Database Management and Database Design
 P4
 Submitted by 
 Sree Achyutha Penmetcha           NUID : 002960283
-Ankita Kumari                     NUID : 
+Ankita Kumari                     NUID : 002929668
 Hazel Lobo						  NUID : 001001740
 Sanjana Chatti                    NUID : 
 Vachana Belgai                    NUID : 
