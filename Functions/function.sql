bbdbb6465e13b92793a5ae456f0f48f18dcd5b86
Achyuth Varma <achyuth.varma1@gmail.com>
2022-08-02 17:53:41 -0400

    Added a couple of addition functions and a single trigger for transactions that has been made between accounts

--- Functions/function.sql
+++ Functions/function.sql
@@ -1,6 +1,7 @@
 --COMPUTE COLUMN BASED ON A FUNCTION
 
 --Calculate Age Column from PersonID and DateOfBirth
+GO
 CREATE FUNCTION calculateAgeFromDOB(@PersonID INT)
 RETURNS INT
 AS
@@ -13,8 +14,45 @@ AS
 
    RETURN @Age
    END
+GO   
 
 ALTER TABLE dbo.Person
 ADD Age AS (dbo.calculateAgeFromDOB(PersonID));
 
 SELECT * FROM Person;
+
+
+-- Function used to add Money
+GO
+CREATE FUNCTION AddMoney(@ValueOne MONEY, @ValueTwo MONEY)
+RETURNS MONEY
+AS
+BEGIN
+
+    DECLARE @Output MONEY;
+
+    SET @Output = 0.0;
+
+    SET @Output = @ValueOne + @ValueTwo;
+
+    RETURN @Output;
+END
+GO
+
+-- Function used to Subtract Money
+GO
+CREATE FUNCTION SubtractMoney(@ValueOne MONEY, @ValueTwo MONEY)
+RETURNS MONEY
+AS
+BEGIN
+
+    DECLARE @Output MONEY;
+
+    SET @Output = 0.0;
+
+    SET @Output = ABS(@ValueOne - @ValueTwo);
+
+    RETURN @Output;
+END
+GO
+
